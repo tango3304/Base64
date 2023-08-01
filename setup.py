99489f490f27678a0d41e04521099f95d9055ba5
tango3304 <94199810+tango3304@users.noreply.github.com>
2023-08-02 01:50:03 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='base64',
+    version='0.1.0',
+    packages=find_packages(),
+    description='bace64 encode and decode',
+        install_requires=[],
+)
