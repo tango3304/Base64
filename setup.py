a950d566e2be4a487e80c7df5660f50ded3c5eab
tango3304 <94199810+tango3304@users.noreply.github.com>
2023-08-02 02:01:51 +0900

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='base64',
-    version='0.1.0',
-    packages=find_packages(),
-    description='bace64 encode and decode',
-        install_requires=[],
-)
